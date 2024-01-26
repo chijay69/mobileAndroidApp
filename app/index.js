6c6fd0861ee6ccd9070bff9b044884998ff42fbe
chijindu.alozie <chijay69@outlook.com>
2024-01-26 12:53:34 +0100

    second

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native-web";
+import React from "react";
+
+const index = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
+export default index;
+
+const style = StyleSheet.create({})
